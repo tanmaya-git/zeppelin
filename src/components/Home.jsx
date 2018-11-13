494b5bc4745c432b5e65cf2d4dc1e09be6b3d8e8
varunk41 <43617370+varunk41@users.noreply.github.com>
2018-11-13 12:34:24 -0800

    home component changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,40 @@
 import React, { Component } from 'react';
-import { Panel, Form, FormGroup, ControlLabel, FormControl, Button,Col } from 'react-bootstrap'
-import SideNav from'./SideNav'
+import { Panel, Form, FormGroup, ControlLabel, FormControl, Button,ButtonToolbar, Col } from 'react-bootstrap'
+import SideNav from './SideNav'
 class Home extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <div >
-                    <Col md = {2}>
-                    <SideNav />
+                    <Col md={2}>
+                        <SideNav />
                     </Col>
-                <Col md={10}>
-                <Panel>
-                    <Panel.Title>
-                        <div className="pull-right" style={{padding:"5px"}}>
-                            <Button>Run</Button>
-                            <Button>Clear</Button>
-                        </div>
-                    </Panel.Title>
-                    <Panel.Body>
-                        <Form>
-                            <FormGroup controlId="formControlsTextarea">
-                                <FormControl componentClass="textarea" placeholder="textarea" />
-                            </FormGroup>
-                        </Form>
-                    </Panel.Body>
-                </Panel>
-           
-           <Button className="btn btn-primary">Add Paragraph</Button>
-           </Col>
-           </div>
-         {/* <Panel>
-             <Panel.body>
-                 
-             </Panel.body>
-         </Panel> */}
-          </div>
-    );
+                    <Col md={10}>
+                        <Panel>
+                            <Panel.Title>
+                                <div className="pull-right" style={{ padding: "5px" }}>
+                                    <ButtonToolbar>
+                                        <Button bsStyle="info" style={{ paddingRight: "10px" }}>Run</Button>
+
+                                        <Button bsStyle="info">Clear</Button>
+                                    </ButtonToolbar>
+                                </div>
+                            </Panel.Title>
+                            <Panel.Body>
+                                <Form>
+                                    <FormGroup controlId="formControlsTextarea">
+                                        <FormControl componentClass="textarea" placeholder="textarea" />
+                                    </FormGroup>
+                                </Form>
+                            </Panel.Body>
+                        </Panel>
+
+                        <Button className="btn btn-primary" pullRight>Add Paragraph</Button>
+                    </Col>
+                </div>
+            </div>
+
+        );
     }
 }
 
